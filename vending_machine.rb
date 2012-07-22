17c92403eed93fc817a483be378d43f53a27f4fb
joker1007 <kakyoin.hierophant@gmail.com>
2012-07-22 11:51:40 +0900

    implement accept_money

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,4 +1,13 @@
 require "bundler/setup"
 
 class VendingMachine
+  attr_accessor :total_accepted_money
+
+  def initialize
+    @total_accepted_money = 0
+  end
+
+  def accept_money(money)
+    @total_accepted_money += money
+  end
 end
--- vending_machine_spec.rb
+++ vending_machine_spec.rb
@@ -1,13 +1,46 @@
-
 require File.dirname(__FILE__) + '/vending_machine'
+
+require "rspec-parameterized"
 
 describe VendingMachine do 
 
   it 'returns total accespted money' do
-    subject.total_accepted_money.should == 0
-    subject.accespt_coint(100)
+    subject.total_accepted_money = 100
     subject.total_accepted_money.should == 100
   end
 
+  describe "#accept_money(money)" do
+    with_them do
+      it "should add money to total_accepted_money" do
+        subject.accept_money(money)
+        subject.total_accepted_money.should == expected
+      end
+    end
+
+    where(:money, :expected) do
+      [
+        [10, 10],
+        [100, 100]
+      ]
+    end
+
+    context "multi accept" do
+      with_them do
+        it "should add money to total_accepted_money" do
+          subject.accept_money(money1)
+          subject.accept_money(money2)
+          subject.total_accepted_money.should == expected
+        end
+      end
+
+      where(:money1, :money2, :expected) do
+        [
+          [10, 10, 20],
+          [100, 10, 110]
+        ]
+      end
+
+    end
+  end
 end
 
